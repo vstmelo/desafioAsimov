73cf450c82fa78ab7a812519dd9e43c03d5eb071
Vmeelo <vinicius.melo@dcomp.ufs.br>
2021-11-26 19:12:31 -0300

    feat: routes.

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,7 +1,14 @@
 //regras para as rotas e seus requests.
 
 const login = async (req, res) => {
-
+    const {body} = req;
+    try{
+        if(body.res.user == "isaac"){
+            console.log("oukay")
+        }
+    }catch(error){
+        console.log(error);
+    }
 }
 
 module.exports = {
